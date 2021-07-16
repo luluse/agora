08ca5b0135b486aa1ced66296485825a12bea597
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2021-07-16 13:11:25 -0700

    login users through postman

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
+//REGISTER
 router.post("/register", async (req, res) => {
 
   try{
@@ -20,9 +21,23 @@ router.post("/register", async (req, res) => {
     const user = await newUser.save();
     res.status(200).json(user)
   } catch(err){
-    console.log(err)
+    res.status(500).json(err)
   }
+});
 
+//LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json("user not found");
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
+    !validPassword && res.status(400).json("wrong password")
+
+    res.status(200).json(user)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
