a81c7a4bc7f9b733694b75bbdafc4d05a69be8cf
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2021-07-16 10:04:34 -0700

    set up user route

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
 
+const userRoute = require("./routes/users");
+
 dotenv.config();
 
 mongoose.connect(
@@ -15,6 +17,18 @@ mongoose.connect(
     console.log("Connected to MongoDB");
   }
 );
+
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+
+// app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+// app.use("/api/posts", postRoute);
+
 
 app.listen(8800, ()=>{
   console.log("Server is running")
