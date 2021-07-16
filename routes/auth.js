2c29e36e36e4e16898fbed646eca64349566a1f5
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2021-07-16 10:16:46 -0700

    created user mongoose model

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
-router.get("/", (req,res)=>{
-  res.send("This is auth route")
+const User = require("../models/User");
+
+router.post("/register",(req, res) => {
+
 })
 
 module.exports = router;
