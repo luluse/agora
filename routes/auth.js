b870a523024ae0cdd745f4a600e3cb34755e59c8
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2021-07-16 10:22:14 -0700

    added a user to db through register page

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,15 @@ const router = require("express").Router();
 
 const User = require("../models/User");
 
-router.post("/register",(req, res) => {
+router.post("/register", async (req, res) => {
+  const user = await new User({
+    username:"lulu",
+    email:"lulu.app@gmail.com",
+    password:"jordi32"
+  })
 
+  await user.save();
+  res.send("ok")
 })
 
 module.exports = router;
