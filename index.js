c211d9010a96dc4097e28c9cd693c5674159f52b
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2021-07-19 10:10:19 -0700

    created posts mongoose schema

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
 
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
 
-
-dotenv.config();
 
 mongoose.connect(
   process.env.MONGODB,
@@ -29,7 +30,7 @@ app.use(morgan("common"));
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
-
+app.use("/api/posts", postRoute);
 
 
 app.listen(8800, ()=>{
