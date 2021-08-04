e38b5f26b47e46287834c7f37b3629e2c18fe670
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2021-08-04 11:24:17 -0700

    can upload images from share component and saves into DB

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ dotenv.config();
 const mongoose = require("mongoose");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const multer = require("multer");
+const path = require("path");
 
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
@@ -20,12 +22,30 @@ mongoose.connect(
   }
 );
 
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 //middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploded successfully");
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
